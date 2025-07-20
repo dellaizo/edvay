ed090bdf50af30ced5bf3e13309b3c1855e878eb
dellaizo <androsiucroman@gmail.com>
2025-07-20 12:18:23 +0200

    Add files via upload

---
+++ preloader.js
@@ -0,0 +1,10 @@
+
+  window.addEventListener('load', function () {
+    const preloader = document.getElementById('preloader');
+    preloader.style.opacity = '0';
+    preloader.style.transition = 'opacity 0.5s ease';
+
+    setTimeout(() => {
+      preloader.style.display = 'none';
+    }, 500);
+  });
